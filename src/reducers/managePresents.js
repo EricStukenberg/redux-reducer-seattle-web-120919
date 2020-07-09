647808a936721fb91ecc007b2f19df0a71af7cdb
stukene <estuken53@gmail.com>
2020-07-09 13:56:33 -0700

    wrote managePresents Reducer

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,7 +1,11 @@
-export function managePresents(state, action){
-    action = { 
-        
+export function managePresents(state ={numberOfPresents: 0} , action){
+    
+    switch(action.type) {
+        case "INCREASE": 
+        return Object.assign({}, state{
+            numberOfPresents: state.numberOfPresents + 1;
+        });
+        default: 
+            return state;
     }
-
-
-}
+};
